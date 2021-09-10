e288568168f6221bc45c86b062c22d88d44707ee
soso-light <80017031+soso-light@users.noreply.github.com>
2021-09-10 11:05:12 +0900

    add students and order it.

---
+++ StudentAppSort/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Student> list = new ArrayList<Student>();
+
+		list.add(new Student(11, "±è¹Î¾Æ", 20););
+		list.add(new Student(12, "ÀÌ¹Î¾Æ", 22););
+		list.add(new Student(13, "ÃÖ¹Î¾Æ", 23));
+		list.add(new Student(14, "ÀÌÁö¾Æ", 26));
+		list.add(new Student(15, "ÇÏ¹Î¿ì", 24));
+		list.add(new Student(16, "ÀÌÁö¿ì", 27));
+		list.add(new Student(17, "¹Ú¹Î¾Æ", 28));
+		list.add(new Student(18, "±ÇÁö¾Æ", 21));
+		list.add(new Student(19, "¾ÈÁöÀ¯", 20));
+		list.add(new Student(10, "ÀÌÁöÀ¯", 19));
+		
+		System.out.println("Student list (ordered by name)");
+		Collections.sort(list);
+		for(Student s: list) {
+			System.out.println(s.toString());
+		}
+		System.out.println("Student list (reverse ordered by name)");
+		Collections.sort(list, Collections.reverseOrder());
+		for(Student s: list) {
+			System.out.println(s.toString());
+		}
+
+	}
+
+}
--- StudentAppSort/src/Student.java
+++ StudentAppSort/src/Student.java
@@ -1,4 +1,38 @@
 
-public class Student {
+public class Student implements Comparable<Student>{
+	private int no;
+	private String name;
+	private int age;
+	
+	public Student(int no, String name, int age) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.age = age;
+	}
+	
+	public Student() {
+		
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		// TODO Auto-generated method stub
+		return name.compareTo(o.getName());
+	}
+
+	@Override
+	public String toString() {
+		return "Student [no=" + no + ", name=" + name +", age=" + age +"]";
+	}
+	
+	
 }
